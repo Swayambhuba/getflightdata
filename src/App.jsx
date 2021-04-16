f5b2aa7836db2fcab8c3880db0bb03b8dad2755a
Swayambhuba <32097743+Swayambhuba@users.noreply.github.com>
2021-04-16 13:51:19 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import SearchRoot from "./pages/search-root";
+import "./styles/main.scss";
+
+
+// Root component
+const App = () => {
+  return (
+    <>
+      <div className="logo">
+      
+      </div>
+      <SearchRoot />
+    </>
+  );
+};
+
+export default App;
